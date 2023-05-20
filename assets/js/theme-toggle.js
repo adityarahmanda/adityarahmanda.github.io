5cf61ec66fe18668d2bab320f24b657520dfdb71
Aditya Rahmanda <arahmanda21@gmail.com>
2023-05-20 13:55:03 +0700

    strip unused css and add popper.min.js, bootstrap.min.js

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,5 +1,5 @@
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
-let theme = sessionStorage.getItem('theme');
+let theme = localStorage.getItem('theme');
 const shapeEl = document.querySelector(".theme-toggle--shape");
 
 function prefersColorTest(systemInitiatedDark) {
@@ -9,12 +9,12 @@ function prefersColorTest(systemInitiatedDark) {
         document.documentElement.setAttribute('data-theme', 'dark');		
         shapeEl.classList.remove("sun");
         shapeEl.classList.add("moon");
-        sessionStorage.setItem('theme', '');
+        localStorage.setItem('theme', '');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         shapeEl.classList.remove("moon");
         shapeEl.classList.add("sun");
-        sessionStorage.setItem('theme', '');
+        localStorage.setItem('theme', '');
     }
 }
 systemInitiatedDark.addEventListener("change", prefersColorTest);
@@ -23,38 +23,38 @@ if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
     shapeEl.classList.remove("sun");
     shapeEl.classList.add("moon");
-	sessionStorage.setItem('theme', 'dark');
+	localStorage.setItem('theme', 'dark');
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
     shapeEl.classList.remove("moon");
     shapeEl.classList.add("sun");
-	sessionStorage.setItem('theme', 'light');
+	localStorage.setItem('theme', 'light');
 }
 
 function toggleTheme()
 {
-	let theme = sessionStorage.getItem('theme');
+	let theme = localStorage.getItem('theme');
     
     if (theme === "dark") {
         document.documentElement.setAttribute('data-theme', 'light');
         shapeEl.classList.remove("moon");
         shapeEl.classList.add("sun");
-		sessionStorage.setItem('theme', 'light');
+		localStorage.setItem('theme', 'light');
     } else if (theme === "light") {
         document.documentElement.setAttribute('data-theme', 'dark');
         shapeEl.classList.remove("sun");
         shapeEl.classList.add("moon");
-        sessionStorage.setItem('theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     } else if (systemInitiatedDark.matches) {
         document.documentElement.setAttribute('data-theme', 'light');
         shapeEl.classList.remove("moon");
         shapeEl.classList.add("sun");
-		sessionStorage.setItem('theme', 'light');
+		localStorage.setItem('theme', 'light');
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         shapeEl.classList.remove("sun");
         shapeEl.classList.add("moon");
-		sessionStorage.setItem('theme', 'dark');
+		localStorage.setItem('theme', 'dark');
     }
 
     document.dispatchEvent(new Event('colorSchemeChanged'));
