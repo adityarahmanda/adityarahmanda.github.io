ce8adc5682b928c938b1bd9d7cd3065d44968c84
Aditya Rahmanda <arahmanda21@gmail.com>
2023-05-20 09:03:52 +0700

    add dark theme toggle

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,55 @@
+let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)"); 
+let theme = sessionStorage.getItem('theme');
+const shapeEl = document.querySelector(".theme-toggle--shape");
+
+function prefersColorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');		
+        sessionStorage.setItem('theme', '');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        sessionStorage.setItem('theme', '');
+    }
+
+    const replacedClass = systemInitiatedDark.matches ? "sun" : "moon";
+    const replacedWithClass = systemInitiatedDark.matches ? "moon" : "sun";
+    shapeEl.classList.replace(replacedClass, replacedWithClass);
+}
+systemInitiatedDark.addEventListener("change", prefersColorTest);
+
+if (theme === "dark") {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	sessionStorage.setItem('theme', 'dark');
+} else if (theme === "light") {
+	document.documentElement.setAttribute('data-theme', 'light');
+	sessionStorage.setItem('theme', 'light');
+}
+
+const replacedClass = theme === "dark" ? "sun" : "moon";
+const replacedWithClass = theme === "dark" ? "moon" : "sun";
+shapeEl.classList.replace(replacedClass, replacedWithClass);
+
+function toggleTheme()
+{
+	let theme = sessionStorage.getItem('theme');
+    
+    if (theme === "dark") {
+        document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+    } else if (theme === "light") {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        sessionStorage.setItem('theme', 'dark');
+    } else if (systemInitiatedDark.matches) {
+        document.documentElement.setAttribute('data-theme', 'light');
+		sessionStorage.setItem('theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+		sessionStorage.setItem('theme', 'dark');
+    }
+
+    const replacedClass = theme === "dark" ? "moon" : "sun";
+    const replacedWithClass = theme === "dark" ? "sun" : "moon";
+    shapeEl.classList.replace(replacedClass, replacedWithClass);
+
+    document.dispatchEvent(new Event('colorSchemeChanged'));
+}
